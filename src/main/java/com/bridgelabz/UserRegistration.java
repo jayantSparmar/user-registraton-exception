6cb8637cb138b8ece322df5f9406450e96fb6828
jayant <jayantparmar420@gmail.com>
2022-04-04 23:02:35 +0530

    Added: First Name in the j-unit exception program.

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -1,5 +1,17 @@
 package com.bridgelabz;
 
-public class UserRegistration {
+/**
+ * User Registration System needs to ensure all validations are in place during the User Entry.
+ * @author jayant singh parmar
+ * @since 04-04-2021
+ */
 
+public class UserRegistration {
+    public boolean checkFName(String firstName) {
+        /**
+         * regex pattern for FirstName
+         */
+        return (firstName.matches("[A-Z][a-z]{3,}"));
+
+    }
 }
